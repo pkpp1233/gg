ef5b57acefce383465c1ff570b44ff97ef4ed94b
PaulK <pkpp1233@gmail.com>
2014-03-22 15:52:17 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gimmegenius::Application.config.secret_key_base = '29fda04cabffa517cbafd71927a46ce84f0a2784087b841d9de05598354fa3810b545ba15a7b73e64781a03bfd064365839f5332e448da855ae5af31bf95148f'
